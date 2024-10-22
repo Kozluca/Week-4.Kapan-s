2b2fdefd13b4ea3fc594394b39168f741a64dafb
Kozluca <okozluca.1@gmail.com>
2024-10-22 22:27:34 +0300

    Proje dosyası ekle.

---
+++ Week-4.Kapanıs/BaseMakine.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4.Kapanıs
+{
+
+    public abstract class BringProductName  // abstract sınıfı tanımladık
+    {
+        public abstract void ProductName();
+
+        public class BaseMakine : BringProductName  // BaseMakine sınıfı Yukarıdaki sınıftan miras aldı
+        {
+            public override void ProductName()
+            { }
+
+            public string Name { get; set; }             // BaseMakine sınıfındaki propertyleri belirledik.
+            public string SerialNumber { get; set; }
+            public string Explanation { get; set; }
+            public DateTime ProductTime = DateTime.Now;
+            public string OperatingSystem { get; set; }
+            public string TrLicence { get; set; }
+            public string Bluetooth { get; set; }
+            public int usb { get; set; }
+            public void Display()  // BaseMakine sınıfından Ekran Cıktısı alacağımız method
+            {
+                Console.WriteLine("Ürünün adı: " + Name);
+                Console.WriteLine("Ürünün seri numarası: " + SerialNumber);
+                Console.WriteLine("Ürünün Açıklaması: " + Explanation);
+                Console.WriteLine("Ürünün üretim tarihi: " + DateTime.Now);
+                Console.WriteLine("Ürünün İşletim sistemi: " + OperatingSystem);
+            }
+            public virtual void PhoneExtra()   // Phone için virtual methoedu kullandık 
+            { }
+            public virtual void LaptopExtra()  // Laptop için  virtual methoedu kullandık 
+            { }
+        }
+        public class Phone : BaseMakine // Phone sınıfı tanımladık ve BaseMakineden Miras aldık
+        {
+            public Phone(string name, string serialnumber, string explanation, DateTime productTime, string operationSystem)
+            {
+                Name = name;                        // Phone sınıfında propertiler belirlendi
+                SerialNumber = serialnumber;
+                Explanation = explanation;
+                ProductTime = productTime;
+                OperatingSystem = operationSystem;
+
+            }
+            public override void PhoneExtra()         // Phone sınıfında override (ezme) yaptık
+            {
+                Console.WriteLine("Ürünün TR Lisansı vardır.");
+            }
+            public override void ProductName()        // Phone sınıfında override (ezme) yaptık (abstract sınıfından)
+            {
+                Console.WriteLine("TELEFON ADI ............Benim Telefon");
+            }
+
+        }
+        public class Laptop : BaseMakine // BaseMakine sınıfından miras alıp Laptop Sınıf oluşturduk
+        {
+            public Laptop(string name, string serialnumber, string explanation, DateTime productTime, string operationSystem, string bluetooth, int usb)
+            {
+                Name = name;                            // Propertyleri belirledik.
+                SerialNumber = serialnumber;
+                Explanation = explanation;
+                ProductTime = productTime;
+                OperatingSystem = operationSystem;
+                Bluetooth = bluetooth;
+                Usb = usb;
+            }
+            public int Usb        //Encapsulation yaptık
+            {
+                get
+                { return usb; }
+                set
+                {
+                    if (value != 2 && value != 4)
+                    {
+                        usb = -1;
+                    }
+                    else
+                        usb = value;
+                }
+            }
+            public override void LaptopExtra()        // Laptop sınıfında override (ezme) yaptık
+            {
+                Console.WriteLine("Ürünün Bluetoothu vardır");
+                Console.WriteLine("Ürünün Usb sayısı: " + usb);
+            }
+            public override void ProductName()          // Laptop sınıfında override (ezme) yaptık (abstract sınıfından)
+            {
+                Console.WriteLine("LAPTOP ADI ................ Benim Laptop");
+            }
+        }
+    }
+
+
+}
---
+++ Week-4.Kapanıs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Week_4.Kapanıs.BringProductName;
+
+namespace Week_4.Kapanıs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        başla:
+            Console.WriteLine("Telefon Üretmek için 1'e  Bilgisayar üretmek için 2'ye basın.");
+            int number = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan bir 1 yada 2 olarak bir sayı aldık ve number a atadık
+            if (number == 1)    // number 1 e eşitse çalışacak şartı yazdık
+            {
+                BaseMakine Phone = new Phone("samsung", "47852", "2025 Üretimi", DateTime.Now, "android"); // BaseMakine sınıfından Phone adında nesne türettik
+                Phone.Display();   // Telefonun ortak özellikleri yazdırdık
+                Phone.PhoneExtra();// Telefonun ekstra özellikleri yazdırdık
+                Phone.ProductName(); // Telefonun adını yazdırdık
+
+            }
+            else if (number == 2) // number 2 ye eşitse çalışacak şartı yazdık
+            {
+                BaseMakine Laptop = new Laptop("Acer", "123547HG5", "Harika Laptop", DateTime.Now, "Windows", "Var", 3); // BaseMakine sınıfından Laptop adında nesne türettik
+                Laptop.Display();                  // Laptopun ortak özellikleri yazdırdık
+                Laptop.LaptopExtra();               // Laptopun ekstra özellikleri yazdırdık
+                Laptop.ProductName();               // Laptopun adını yazdırdık
+            }
+
+            Console.WriteLine("Ürün Başarı ile üretildi.   Başka bir ürün üretmek ister misiniz?");
+            string answer = Console.ReadLine();
+            answer = answer.ToLower();
+            if (answer == "evet")
+                goto başla;  // cevap evet ise en basa gitmeyi sağladık.
+
+            else if (answer == "hayır")
+                Console.WriteLine("İyi Günler ..........");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
